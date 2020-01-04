1c697640b38f02892df75c7ac21844f65e601345
Lucas Sandeville <lucas.sandeville@gmail.com>
2020-01-04 17:31:41 -0300

    created new react front end with npm

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GSignInBtn from "./react-components/GSignInBtn";
+import GsignInBtn from "./react-components/GSignInBtn";
+
+const App = () => {
+  return (
+    <div class="row justify-content-center align-items-center mw-100 min-vh-100 body-green">
+      <GsignInBtn />
+    </div>
+  );
+};
+
+export default App;
